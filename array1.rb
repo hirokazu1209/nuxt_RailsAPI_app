16ceb64366e18f2a1d8db24ac8fac7f5207e10cd
hirokazu <n.hirokazu92@gmail.com>
2022-01-16 22:38:02 +0900

    array1

---
+++ array1.rb
@@ -0,0 +1,11 @@
+# https://paiza.jp/works/mondai/prob60/fortune_telling_0
+
+user2blood = {
+  'Kyoto' => 'B',
+  'Rio' => 'O',
+  'Tsubame' => 'AB',
+  'kurodaSensei' => 'A',
+  'NekoSensei' => 'A'
+}
+
+user2blood.each { |user, blood| puts "#{user} #{blood} " }
